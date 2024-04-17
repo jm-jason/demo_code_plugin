bd3a1ff3ee786e1c383646060d1647cd8adcb796
GitHub Action <action@github.com>
2024-04-17 15:40:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/jasonmao87/democodeplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jasonmao87/democodeplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jasonmao87.democodeplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jasonmao87/democodeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jasonmao87/democodeplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jasonmao87.democodeplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jasonmao87.democodeplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
